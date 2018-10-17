733ba619732077c8323ddc17c56853ed3dd924cf
T <tbuonogv@gmail.com>
2018-10-16 22:31:59 -0400

    Turkey LED Proximity Test

--- ArduinoNano/ProxitySensorTest/ProxitySensorTest.ino
+++ ArduinoNano/ProxitySensorTest/ProxitySensorTest.ino
@@ -1,10 +1,19 @@
 
 int trigPin = 11;    //Trig - green Jumper
 int echoPin = 12;    //Echo - yellow Jumper
-int onboardLEDPin = 13; // D13 onboard Arduino Nano LED
-int offboardLEDPin = 3; // Offboard LED - D3 --/\/\/\----|>|----Gnd
-//////////////////////                         470 ohm
-long duration, cm, inches;
+
+
+//int onboardLEDPin = 13; // D13 onboard Arduino Nano LED
+//int offboardLEDPin = 3; // Offboard LED - D3 --/\/\/\----|>|----Gnd
+                        //                     470 ohm
+
+int pinFeather0 = 2;
+int pinFeather1 = 3;
+int pinFeather2 = 4;
+int pinFeather3 = 5;
+
+long currentInches;
+int prevFeathers;
 
 void setup() {
   //Serial Port begin
@@ -12,11 +21,114 @@ void setup() {
   //Define inputs and outputs
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(onboardLEDPin, OUTPUT);
-  pinMode(offboardLEDPin, OUTPUT);
-  digitalWrite(onboardLEDPin, LOW);
-  digitalWrite(offboardLEDPin, LOW);
+  
+  //pinMode(onboardLEDPin, OUTPUT);
+  //pinMode(offboardLEDPin, OUTPUT);
+
+  pinMode(pinFeather0, OUTPUT);
+  pinMode(pinFeather1, OUTPUT);
+  pinMode(pinFeather2, OUTPUT);
+  pinMode(pinFeather3, OUTPUT);
+
+  //digitalWrite(onboardLEDPin, LOW);
+  //digitalWrite(offboardLEDPin, LOW);
+  digitalWrite(pinFeather0, LOW);
+  digitalWrite(pinFeather1, LOW);
+  digitalWrite(pinFeather2, LOW);
+  digitalWrite(pinFeather3, LOW);
+
+  // initialize variables
+  currentInches = 0;
+  prevFeathers = 0;
 }
+
+
+
+int inchesToFeathers(long i) {
+
+  if(i > 40) {
+    return 0;
+    
+  } else if(i > 25) {
+    return 1;
+    
+  } else if(i > 16) {
+    return 2;
+    
+  } else if(i > 8) {
+    return 3;
+    
+  } else {
+    return 4;
+  }
+  
+}
+
+
+void setFeathers(int i) {
+
+  int n = i;
+
+  if(n > 4) {
+    n = 4;
+    
+  } else if(n < 0) {
+    n = 0;
+    
+  } else {
+    // keep n as-is
+  }
+
+
+
+  switch(n) {
+    case 0:
+      digitalWrite(pinFeather0, LOW);
+      digitalWrite(pinFeather1, LOW);
+      digitalWrite(pinFeather2, LOW);
+      digitalWrite(pinFeather3, LOW);
+      break;
+      
+    case 1:
+      digitalWrite(pinFeather0, HIGH);
+      digitalWrite(pinFeather1, LOW);
+      digitalWrite(pinFeather2, LOW);
+      digitalWrite(pinFeather3, LOW);
+      break;
+      
+    case 2:
+      digitalWrite(pinFeather0, HIGH);
+      digitalWrite(pinFeather1, HIGH);
+      digitalWrite(pinFeather2, LOW);
+      digitalWrite(pinFeather3, LOW);
+      break;   
+
+    case 3:
+      digitalWrite(pinFeather0, HIGH);
+      digitalWrite(pinFeather1, HIGH);
+      digitalWrite(pinFeather2, HIGH);
+      digitalWrite(pinFeather3, LOW);
+      break; 
+
+    case 4:
+      digitalWrite(pinFeather0, HIGH);
+      digitalWrite(pinFeather1, HIGH);
+      digitalWrite(pinFeather2, HIGH);
+      digitalWrite(pinFeather3, HIGH);
+      break;
+      
+    default:
+      digitalWrite(pinFeather0, LOW);
+      digitalWrite(pinFeather1, LOW);
+      digitalWrite(pinFeather2, LOW);
+      digitalWrite(pinFeather3, LOW);
+      break;
+  }
+  
+}
+
+
+
 
 void loop()
 {
@@ -32,31 +144,31 @@ void loop()
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
   pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
+  long duration = pulseIn(echoPin, HIGH);
  
-  // convert the time into a distance
-  cm = (duration/2) / 29.1;
-  inches = (duration/2) / 74;
+  // convert the time into a distance (in inches)
+  currentInches = (duration / 2) / 74;
 
-  if(inches > 30) {
-    digitalWrite(onboardLEDPin, HIGH);
-    digitalWrite(offboardLEDPin, HIGH);
-  } else if(inches > 20) {
-    digitalWrite(onboardLEDPin, HIGH);
-    digitalWrite(offboardLEDPin, LOW);
-  } else if(inches > 10) {
-    digitalWrite(onboardLEDPin, LOW);
-    digitalWrite(offboardLEDPin, HIGH);
+  //Serial.print("current=");
+  //Serial.print(currentInches);
+  //Serial.print(", prevF=");
+  //Serial.print(prevFeathers);
+  //Serial.println();
+
+  if(inchesToFeathers(currentInches) > prevFeathers) {
+    int numFeathers = prevFeathers + 1;
+    setFeathers(numFeathers);
+    prevFeathers = numFeathers;
+    
+  } else if(inchesToFeathers(currentInches) < prevFeathers) {
+    int numFeathers = prevFeathers - 1;
+    setFeathers(numFeathers);
+    prevFeathers = numFeathers;
+    
   } else {
-    digitalWrite(onboardLEDPin, LOW);
-    digitalWrite(offboardLEDPin, LOW);
+    // Do nothing.  Keep the number of feathers the same.
   }
+
   
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  
-  delay(1000);
+  delay(250);
 }
